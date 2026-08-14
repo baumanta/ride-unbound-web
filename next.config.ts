80e654fb2d8baf06c7a3cc15d414999ab9e555c6
Tanja Baumann <baumann.tanja@outlook.com>
2026-08-14 22:09:19 +0200

    add github actions for building and serving page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/ride-unbound-web" : "",
+  assetPrefix: isProd ? "/ride-unbound-web/" : "",
 };
 
 export default nextConfig;
